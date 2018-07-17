80628b152e0228ef8e716f5d7d9e2594f7ef54b3
co2-git <francoisrvespa@gmail.com>
2018-07-17 15:50:23 +0200

    save

--- src/actions/deleteMany.ts
+++ src/actions/deleteMany.ts
@@ -6,9 +6,11 @@ import { InputProps, ResponseDocument } from '../types';
 
 const deleteMany = async (collection: Collection, params: InputProps): Promise<ResponseDocument[]> => {
   const docs = await findMany(collection, params)
-  const ids = map(map(docs, 'id'), ObjectID)
+  const ids = map(docs, 'id')
   console.log(ids)
-  const res = await collection.deleteMany({ _id: { $in: ids } })
+  const objectIds = map(ids, ObjectID)
+  console.log(objectIds)
+  const res = await collection.deleteMany({ _id: { $in: objectIds } })
   console.log(res)
   return docs
 }
