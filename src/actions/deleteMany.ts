7574296d9ddb2bfa4eb652f70356e832781e3dda
co2-git <francoisrvespa@gmail.com>
2018-07-17 15:53:26 +0200

    save

--- src/actions/deleteMany.ts
+++ src/actions/deleteMany.ts
@@ -1,4 +1,4 @@
-import { Collection, ObjectID } from 'mongodb'
+import { Collection } from 'mongodb'
 import { map } from 'lodash'
 
 import findMany from './findMany'
@@ -7,11 +7,9 @@ import { InputProps, ResponseDocument } from '../types';
 const deleteMany = async (collection: Collection, params: InputProps): Promise<ResponseDocument[]> => {
   const docs = await findMany(collection, params.input)
   console.log(docs)
-  const ids = map(docs, 'id')
+  const ids = map(docs, '_id')
   console.log(ids)
-  const objectIds = map(ids, ObjectID)
-  console.log(objectIds)
-  const res = await collection.deleteMany({ _id: { $in: objectIds } })
+  const res = await collection.deleteMany({ _id: { $in: ids } })
   console.log(res)
   return docs
 }
