37c62818d3f5bdb13e2f3451a15d4dc4ef92abde
co2-git <francoisrvespa@gmail.com>
2018-07-17 15:48:27 +0200

    save

--- src/actions/deleteMany.ts
+++ src/actions/deleteMany.ts
@@ -1,4 +1,4 @@
-import { Collection } from 'mongodb'
+import { Collection, ObjectID } from 'mongodb'
 import { map } from 'lodash'
 
 import findMany from './findMany'
@@ -6,7 +6,8 @@ import { InputProps, ResponseDocument } from '../types';
 
 const deleteMany = async (collection: Collection, params: InputProps): Promise<ResponseDocument[]> => {
   const docs = await findMany(collection, params)
-  const ids = map(docs, 'id')
+  const ids = map(map(docs, 'id'), ObjectID)
+  console.log(ids)
   const res = await collection.deleteMany({ _id: { $in: ids } })
   console.log(res)
   return docs
--- src/actions/updateOne.ts
+++ src/actions/updateOne.ts
@@ -4,7 +4,7 @@ import { InputProps, OutputProps, ResponseDocument } from '../types'
 
 const updateOne = async (collection: Collection, params: InputProps & OutputProps)
 : Promise<ResponseDocument | undefined> => {
-  const res = await collection.findOneAndUpdate(params.input, { $set: params.output })
+  const res = await collection.findOneAndUpdate(params.input, { $set: params.output, upsert: false })
   if (!res.ok) {
     return undefined
   }
